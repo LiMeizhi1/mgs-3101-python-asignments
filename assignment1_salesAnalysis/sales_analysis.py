02227dae928663838ca7de538ab74c6969d4b7c5
羊 <shidan@yangdeMacBook-Air.local>
2026-09-24 21:11:01 -0400

    Add revenue calculation and condition

--- assignment1_salesAnalysis/sales_analysis.py
+++ assignment1_salesAnalysis/sales_analysis.py
@@ -1,5 +1,15 @@
-shop_name="Lower Manhattan"
-drinks_sold=57518
-price_per_drink=3.05
-pastries_sold=8039
-price_per_pastry=3.52
+shop_name = "Lower Manhattan"
+drinks_sold = 57518
+price_per_drink = 3.05
+pastries_sold = 8039
+price_per_pastry = 3.52
+drink_revenue=price_per_drink*drinks_sold
+pastry_revenue = price_per_pastry * pastries_sold
+total_revenue = drink_revenue + pastry_revenue
+print(drink_revenue)
+print(pastry_revenue)
+print(total_revenue)
+if total_revenue >= 500:
+    print("Total revenue is at least $500")
+else:
+    print("Total revenue is less than $500")
